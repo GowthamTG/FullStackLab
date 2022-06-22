7c871938be390843908c1582f6ce9a133d61d6d4
Gowtham T G <gowthamgopinath1@gmail.com>
2022-06-22 20:57:12 +0530

    Tasks Done

---
+++ task24/src/components/Task/Task.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./Task.css";
+
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.removeTask = this.removeTask.bind(this);
+    this.markDone = this.markDone.bind(this);
+
+    this.state = {
+      backgroundColor: "",
+      textDecorationLine: "",
+      textDecorationStyle: "",
+    };
+  }
+
+  removeTask() {
+    // this.props.removeTask(this.props.id);
+    if (this.state.backgroundColor === "") {
+      this.setState({
+        backgroundColor: "rgb(252, 81, 129)",
+        textDecorationLine: "",
+        textDecorationStyle: "",
+      });
+    } else {
+      this.setState({
+        backgroundColor: "",
+        textDecorationLine: "",
+        textDecorationStyle: "",
+      });
+    }
+  }
+
+  markDone() {
+    if (this.state.backgroundColor === "") {
+      this.setState({
+        backgroundColor: "rgb(144,238,144, 0.5)",
+        textDecorationLine: "line-through",
+        textDecorationStyle: "solid",
+      });
+    } else {
+      this.setState({
+        backgroundColor: "",
+        textDecorationLine: "",
+        textDecorationStyle: "",
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="task-container">
+        <div
+          className="task-container-background"
+          style={{
+            backgroundColor: this.state.backgroundColor,
+            textDecorationLine: this.state.textDecorationLine,
+            textDecorationStyle: this.state.textDecorationStyle,
+          }}
+        >
+          <div className="task-term-container">
+            <h2 className="task-term">{this.props.text}</h2>
+          </div>
+          <div className="buttons-container">
+            <button className="done-button" onClick={this.markDone}>
+              v
+            </button>
+            <button className="delete-button" onClick={this.removeTask}>
+              x
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Task;
